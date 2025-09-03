9b15ef342f65bd4e17809d2d93940a7aaf4d4a97
romaina229 <nounonvenanromain@gmail.com>
2025-09-03 12:56:36 +0100

    finale

--- Semaine_6/assets/js/script.js
+++ Semaine_6/assets/js/script.js
@@ -1,39 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('contact-form');
-    var confirmation = document.getElementById('confirmation');
-    if(form) {
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-            // Validation simple
-            var nom = form.nom.value.trim();
-            var email = form.email.value.trim();
-            var sujet = form.sujet.value.trim();
-            var message = form.message.value.trim();
-            if(nom.length < 2) {
-                alert('Veuillez entrer un nom valide.');
-                return;
-            }
-            if(!email.match(/^\S+@\S+\.\S+$/)) {
-                alert('Veuillez entrer un email valide.');
-                return;
-            }
-            if(message.length < 5) {
-                alert('Votre message est trop court.');
-                return;
-            }
-            form.style.display = 'none';
-            confirmation.style.display = 'block';
-        });
-    }
-});
-
-const toggleBtn = document.querySelector(".menu-toggle");
-  const navMenu = document.querySelector("nav ul");
-
-  toggleBtn.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
-
+// script.js
+// Sélection du formulaire
   document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault(); // Empêche le rechargement de la page
 
