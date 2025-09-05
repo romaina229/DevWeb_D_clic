72124f6977cdce03e1877f5302d78385303c3b8d
romaina229 <nounonvenanromain@gmail.com>
2025-09-05 14:22:30 +0100

    actualisation de données

--- lifero/srcipt.js
+++ lifero/srcipt.js
@@ -1,3 +1,15 @@
 function toggleSideMenu() {
     document.getElementById('sideMenu').classList.toggle('open');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+        var form = document.getElementById('form-prestation');
+        var confirmation = document.getElementById('confirmation-message');
+        if(form) {
+            form.addEventListener('submit', function(e) {
+                e.preventDefault();
+                form.style.display = 'none';
+                confirmation.style.display = 'block';
+            });
+        }
+    });
